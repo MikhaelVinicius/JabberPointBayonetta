197e6857ce5cbd32c37cfb6dfabdc5b8ff046e22
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-11 03:25:34 -0300

    Cores alteradas

--- src/main/java/View/Style.java
+++ src/main/java/View/Style.java
@@ -16,11 +16,11 @@ public class Style {
 
   public static void createStyles() {
     styles = new Style[5];
-    styles[0] = new Style(0, Color.red, 48, 20); // nível 0
-    styles[1] = new Style(20, Color.blue, 40, 10); // nível 1
-    styles[2] = new Style(50, Color.black, 36, 10); // nível 2
-    styles[3] = new Style(70, Color.black, 30, 10); // nivel 3
-    styles[4] = new Style(90, Color.black, 24, 10); // nível 4
+    styles[0] = new Style(0, new Color(141, 107, 227), 48, 20); // nível 0
+    styles[1] = new Style(20, new Color(155, 89, 182), 40, 10); // nível 1
+    styles[2] = new Style(50, new Color(184, 80, 147), 36, 10); // nível 2
+    styles[3] = new Style(70, new Color(218, 67, 98), 30, 10); // nivel 3
+    styles[4] = new Style(90, new Color(242, 38, 19), 24, 10); // nível 4
   }
 
   public static Style getStyle(int level) {
